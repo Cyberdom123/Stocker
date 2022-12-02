757ef77b80606ea05c881d831edb3815d9933892
root <root@DESKTOP-HKB1I8E>
2022-12-02 19:29:07 +0100

    cmake, database, gui tests

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,72 @@
+#include <gtkmm/button.h>
+#include <gtkmm/window.h>
+#include <gtkmm/application.h>
 #include <iostream>
-#include "testLib.h"
+#include <vector>
+#include <sqlite3.h> 
 
-int main(){
-    testLib test1;
-    test1.Print();
+class HelloWorld : public Gtk::Window
+{
+
+public:
+  HelloWorld();
+  ~HelloWorld() override;
+
+protected:
+  //Signal handlers:
+  void on_button_clicked();
+
+  //Member widgets:
+  Gtk::Button m_button;
+};
+
+
+HelloWorld::HelloWorld()
+: m_button("Hello World")   // creates a new button with label "Hello World".
+{
+  // Sets the margin around the button.
+  m_button.set_margin(10);
+
+  // When the button receives the "clicked" signal, it will call the
+  // on_button_clicked() method defined below.
+  m_button.signal_clicked().connect(sigc::mem_fun(*this,
+              &HelloWorld::on_button_clicked));
+
+  // This packs the button into the Window (a container).
+  set_child(m_button);
+}
+
+HelloWorld::~HelloWorld()
+{
+}
+
+void HelloWorld::on_button_clicked()
+{
+  std::cout << "Hello World" << std::endl;
+  std::vector<std::string> test;
+  test.push_back("test");
+  std::cout << test[0] << std::endl;
+ }
+
+
+int main(int argc, char* argv[])
+{
+    sqlite3 *db;
+    char *zErrMsg = 0;
+
+     
+    int rc = sqlite3_open("test.db", &db);
+
+    if( rc ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(0);
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+    sqlite3_close(db);
+
+    auto app = Gtk::Application::create("org.gtkmm.example");
+
+    //Shows the window and returns when it is closed.
+    return app->make_window_and_run<HelloWorld>(argc, argv);
 }
