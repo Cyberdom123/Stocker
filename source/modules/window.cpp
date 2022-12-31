1faaa229c7d34cdcec6ddc96dedcca709eaccc59
Cyberdom123 <dmichalczyk73@gmail.com>
2022-12-31 10:28:47 +0100

    list view

--- source/modules/window.cpp
+++ source/modules/window.cpp
@@ -1,28 +1,88 @@
 #include "window.h"
 
-Window::Window()
-: m_button("Hello World")   // creates a new button with label "Hello World".
+ExampleWindow::ExampleWindow()
+: m_VBox(Gtk::Orientation::VERTICAL),
+  m_Button_Quit("Quit")
 {
-  // Sets the margin around the button.
-  m_button.set_margin(10);
+  set_title("Gtk::TreeView (ListStore) example");
+  set_default_size(400, 200);
 
-  // When the button receives the "clicked" signal, it will call the
-  // on_button_clicked() method defined below.
-  m_button.signal_clicked().connect(sigc::mem_fun(*this,
-              &Window::on_button_clicked));
+  m_VBox.set_margin(5);
+  set_child(m_VBox);
 
-  // This packs the button into the Window (a container).
-  set_child(m_button);
+  //Add the TreeView, inside a ScrolledWindow, with the button underneath:
+  m_ScrolledWindow.set_child(m_TreeView);
+
+  //Only show the scrollbars when they are necessary:
+  m_ScrolledWindow.set_policy(Gtk::PolicyType::AUTOMATIC, Gtk::PolicyType::AUTOMATIC);
+  m_ScrolledWindow.set_expand();
+
+  m_VBox.append(m_ScrolledWindow);
+  m_VBox.append(m_ButtonBox);
+
+  m_ButtonBox.append(m_Button_Quit);
+  m_ButtonBox.set_margin(5);
+  m_Button_Quit.set_hexpand(true);
+  m_Button_Quit.set_halign(Gtk::Align::END);
+  m_Button_Quit.signal_clicked().connect( sigc::mem_fun(*this,
+              &ExampleWindow::on_button_quit) );
+
+  //Create the Tree model:
+  m_refTreeModel = Gtk::ListStore::create(m_Columns);
+  m_TreeView.set_model(m_refTreeModel);
+
+  //Fill the TreeView's model
+  auto row = *(m_refTreeModel->append());
+  row[m_Columns.m_col_id] = 1;
+  row[m_Columns.m_col_name] = "Billy Bob";
+  row[m_Columns.m_col_number] = 10;
+  row[m_Columns.m_col_percentage] = 15;
+
+  row = *(m_refTreeModel->append());
+  row[m_Columns.m_col_id] = 2;
+  row[m_Columns.m_col_name] = "Joey Jojo";
+  row[m_Columns.m_col_number] = 20;
+  row[m_Columns.m_col_percentage] = 40;
+
+  row = *(m_refTreeModel->append());
+  row[m_Columns.m_col_id] = 3;
+  row[m_Columns.m_col_name] = "Rob McRoberts";
+  row[m_Columns.m_col_number] = 30;
+  row[m_Columns.m_col_percentage] = 70;
+
+  //Add the TreeView's view columns:
+  //This number will be shown with the default numeric formatting.
+  m_TreeView.append_column("ID", m_Columns.m_col_id);
+  m_TreeView.append_column("Name", m_Columns.m_col_name);
+
+  m_TreeView.append_column_numeric("Formatted number", m_Columns.m_col_number,
+          "%010d" /* 10 digits, using leading zeroes. */);
+
+  //Display a progress bar instead of a decimal number:
+  auto cell = Gtk::make_managed<Gtk::CellRendererProgress>();
+  int cols_count = m_TreeView.append_column("Some percentage", *cell);
+  auto pColumn = m_TreeView.get_column(cols_count - 1);
+  if(pColumn)
+  {
+    pColumn->add_attribute(cell->property_value(), m_Columns.m_col_percentage);
+  }
+
+  //Make all the columns reorderable:
+  //This is not necessary, but it's nice to show the feature.
+  //You can use TreeView::set_column_drag_function() to more
+  //finely control column drag and drop.
+  for(guint i = 0; i < 2; i++)
+  {
+    auto column = m_TreeView.get_column(i);
+    column->set_reorderable();
+  }
 }
 
-Window::~Window()
+ExampleWindow::~ExampleWindow()
 {
 }
 
-void Window::on_button_clicked()
+void ExampleWindow::on_button_quit()
 {
-  std::cout << "Hello World" << std::endl;
-  std::vector<std::string> test;
-  test.push_back("test");
-  std::cout << test[0] << std::endl;
- }
+  hide();
+}
--- source/modules/window.h
+++ source/modules/window.h
@@ -4,17 +4,41 @@
 
 #pragma once
 
-class Window : public Gtk::Window
+
+class ExampleWindow : public Gtk::Window
 {
 public:
-  Window();
-  ~Window() override;
+  ExampleWindow();
+  virtual ~ExampleWindow();
 
 protected:
   //Signal handlers:
-  void on_button_clicked();
+  void on_button_quit();
 
-  //Member widgets:
-  Gtk::Button m_button;
+  //Tree model columns:
+  class ModelColumns : public Gtk::TreeModel::ColumnRecord
+  {
+  public:
+
+    ModelColumns()
+    { add(m_col_id); add(m_col_name); add(m_col_number); add(m_col_percentage);}
+
+    Gtk::TreeModelColumn<unsigned int> m_col_id;
+    Gtk::TreeModelColumn<Glib::ustring> m_col_name;
+    Gtk::TreeModelColumn<short> m_col_number;
+    Gtk::TreeModelColumn<int> m_col_percentage;
+  };
+
+  ModelColumns m_Columns;
+
+  //Child widgets:
+  Gtk::Box m_VBox;
+
+  Gtk::ScrolledWindow m_ScrolledWindow;
+  Gtk::TreeView m_TreeView;
+  Glib::RefPtr<Gtk::ListStore> m_refTreeModel;
+
+  Gtk::Box m_ButtonBox;
+  Gtk::Button m_Button_Quit;
 };
 
