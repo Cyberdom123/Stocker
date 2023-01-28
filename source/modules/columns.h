49a69ce72b585745896144ec4f8959cc35d1b995
Yoshua-chan <31251884+Yoshua-chan@users.noreply.github.com>
2023-01-28 16:56:17 +0100

    fixed typo

--- source/modules/columns.h
+++ source/modules/columns.h
@@ -7,7 +7,7 @@ class ModelColumns : public Gtk::TreeModel::ColumnRecord
 {
     public:
     ModelColumns() {add(m_col_id); add(m_col_name); add(m_col_price); add(m_col_max_quantity);
-                    add(m_col_quantity); add(m_col_sales); add(m_col_purchases); add(m_col_descryption);};
+                    add(m_col_quantity); add(m_col_sales); add(m_col_purchases); add(m_col_description);};
 
     Gtk::TreeModelColumn<unsigned int> m_col_id;
     Gtk::TreeModelColumn<Glib::ustring> m_col_name;
@@ -16,5 +16,5 @@ class ModelColumns : public Gtk::TreeModel::ColumnRecord
     Gtk::TreeModelColumn<int> m_col_quantity;
     Gtk::TreeModelColumn<int> m_col_sales;
     Gtk::TreeModelColumn<int> m_col_purchases;
-    Gtk::TreeModelColumn<Glib::ustring> m_col_descryption;
+    Gtk::TreeModelColumn<Glib::ustring> m_col_description;
 };
